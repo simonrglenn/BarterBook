f28b592c5d822edd8fd8fa7d65ec04923f3b4fe6
Simon R. Glenn <simonrglenn@gmail.com>
2019-11-26 19:57:41 -0500

    created all necessary component files

--- BarterBook/src/components/Currency.js
+++ BarterBook/src/components/Currency.js
@@ -7,6 +7,7 @@ const Currency = (props) => {
             <img src={props.img_url}/>
             <h4>{props.title}</h4>
             <p>{props.text}</p>
+            <button>Propose a Barter!</button>
         </div>
     )
         
